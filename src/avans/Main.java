1c330030c9ed15c36dd0bea1c220cc8cce08e29d
Maurice Snoeren <mac.snoeren@avans.nl>
2020-01-29 13:55:50 +0100

    Added again another console output.

--- src/avans/Main.java
+++ src/avans/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo klas A!");
         System.out.println("Kunnen jullie het bijhouden?!?!?!");
+        System.out.println("Kunnen jullie het bijhouden?!?!?!");
     }
 }
