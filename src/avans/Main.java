495d36ba1e9dc138cb02bbbbfa877a64f39bc98e
Maurice Snoeren <mac.snoeren@avans.nl>
2020-01-29 13:53:42 +0100

    Initial commit

---
+++ src/avans/Main.java
@@ -0,0 +1,9 @@
+package avans;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo klas A!");
+        System.out.println("Kunnen jullie het bijhouden?!?!?!");
+    }
+}
