7c4046f198670e8d5bb2b591bcef15800321143f
Maurice Snoeren <mac.snoeren@avans.nl>
2020-02-05 10:08:06 +0100

    Added method multuply

--- src/avans/Main.java
+++ src/avans/Main.java
@@ -7,11 +7,19 @@ public class Main {
 
         double a = 12.5;
         double b = 88.8;
-        double r = sum(a, b);
-        System.out.println(a + " + " + b + " = " + r);
+        double r1 = sum(a, b);
+        System.out.println(a + " + " + b + " = " + r1);
+
+        double r2 = multiply(a, b);
+        System.out.println(a + " * " + b + " = " + r2);
+
     }
 
     public static double sum (double a, double b) {
         return a + b;
+    }
+
+    public static double multiply (double a, double b) {
+        return a * b;
     }
 }
