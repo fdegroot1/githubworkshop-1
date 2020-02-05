4b725f74688c0f008ab7323ccfad645a86a941a9
Maurice Snoeren <mac.snoeren@avans.nl>
2020-02-05 10:57:35 +0100

    empty

--- src/avans/Main.java
+++ src/avans/Main.java
@@ -5,21 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test my methods:");
 
-        double a = 12.5;
-        double b = 88.8;
-        double r1 = sum(a, b);
-        System.out.println(a + " + " + b + " = " + r1);
-
-        double r2 = multiply(a, b);
-        System.out.println(a + " * " + b + " = " + r2);
-
-    }
-
-    public static double sum (double a, double b) {
-        return a + b;
-    }
-
-    public static double multiply (double a, double b) {
-        return a * b;
     }
 }
