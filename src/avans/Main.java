2d8750e5aafaaa87561357e76027a1bf0ae4c48a
Maurice Snoeren <mac.snoeren@avans.nl>
2020-02-05 09:22:53 +0100

    Added sum method and test code o the main

--- src/avans/Main.java
+++ src/avans/Main.java
@@ -3,12 +3,15 @@ package avans;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas A!");
-        System.out.println("Kunnen jullie het bijhouden?!?!?!");
-        System.out.println("Kunnen jullie het bijhouden?!?!?!");
-        System.out.println("Kunnen jullie het bijhouden?!?!?!");
-        System.out.println("Kunnen jullie het bijhouden?!?!?!");
-        System.out.println("Groetjes van Twan!");
-		System.out.println("Nogmaals groetjes van Twan!");
+        System.out.println("Test my methods:");
+
+        double a = 12.5;
+        double b = 88.8;
+        double r = sum(a, b);
+        System.out.println(a + " + " + b + " = " + r);
+    }
+
+    public static double sum (double a, double b) {
+        return a + b;
     }
 }
