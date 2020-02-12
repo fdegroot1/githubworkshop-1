1919f3d9b5cc236d75610d3b2ca1b5d6c5fe7cbd
Maurice Snoeren <mac.snoeren@avans.nl>
2020-02-12 09:47:29 +0100

    Added code to the Main.java

--- src/avans/Main.java
+++ src/avans/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test my methods:");
 
+        double a = 33.3;
+        double b = 66.6;
+        double r = sum(a, b);
+        System.out.println(a + " + " + b + " = " + r);
+    }
+
+    public static double sum ( double a, double b ) {
+        return a + b;
     }
 }
