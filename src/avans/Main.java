6a8c606d77689a424645051befd8513eda3d42b6
Fabian <f.degroot1@student.avans.nl>
2020-02-12 11:05:44 +0100

    For Loop

--- src/avans/Main.java
+++ src/avans/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test my methods:");
+        for(int i=0; i<500; i++){
+            System.out.println(i);
+        }
 
     }
 }
